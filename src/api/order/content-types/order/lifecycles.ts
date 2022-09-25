331b22f4e1d51b50a0f7091df8785c00aec291c7
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-09-25 13:53:08 +0300

    add  lifecycles order

---
+++ src/api/order/content-types/order/lifecycles.ts
@@ -0,0 +1,13 @@
+const getRandomNumber = (min = 1, max = 10) => {
+  return Math.ceil(Math.random() * (max - min + 1));
+
+};
+export default {
+  beforeCreate(event) {
+    const date = new Date();
+    console.log(date);
+    event.params.data.code = `${date.getFullYear()}
+    ${date.getMonth() + 1
+    }${date.getDate()}-${getRandomNumber()}${getRandomNumber()}${getRandomNumber()}`;
+  }
+};
